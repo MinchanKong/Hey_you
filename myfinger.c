ecfbf6d681a5736265b73c1d051258273b76e3d8
Minchan Kong <rhdalscks@naver.com>
2017-12-04 13:07:26 +0900

    "Recently"

--- myfinger.c
+++ myfinger.c
@@ -3,29 +3,27 @@
 #include <string.h>
 #include <unistd.h>
 #include <utmpx.h>
-#include <stdbool.h>
 #include <dirent.h>
 #include <pwd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <siginfo.h>
 #include <time.h>
+#include <memory.h>
+#include <stdbool.h>
 
 struct COMMAND{ // 커맨드 구조체
         char* name;
         char* desc;
         bool ( *func )( int argc, char* argv[] ); // 함수포인터. 사용할 함수들의 매개변수를 맞춰줌
-}
-;
-pid_t child=-1;                                            // 자식프로세스 pid 저장 전역변수
-int status;                                                  // 프로세스 status
+};
 
 bool cmd_cd( int argc, char* argv[] );          //cd 명령어
 bool cmd_exit( int argc, char* argv[] );        //exit, quit 명령어
 bool cmd_help( int argc, char* argv[] );       //help 명령어
 bool cmd_myfinger(int argc, char* argv[]);     //myfinger 명령어 
+bool cmd_mkdir( int argc, char* argv[] );	// mkdir 명령어
+bool cmd_rmdir( int argc, char* argv[] );	// rmdir 명령어
+bool cmd_ls(int argc, char* argv[]);	// ls 명령어
 
 struct COMMAND  builtin_cmds[] =
 {
@@ -34,22 +32,26 @@ struct COMMAND  builtin_cmds[] =
     { "quit",   "quit this shell",                        cmd_exit  },
     { "help",  "show this help",                      cmd_help },
     { "?",      "show this help",                      cmd_help },
-    { "myfinger", "show user information", cmd_myfinger }
+    { "myfinger", "show user information", cmd_myfinger },
+    { "mkdir", "make directory", cmd_mkdir },
+    { "rmdir", "delete directory", cmd_rmdir },
+    { "ls", "show all files in this directory" ,cmd_ls }
 };
 
 bool cmd_cd( int argc, char* argv[] ){ //cd : change directory
         if( argc == 1 )
                 chdir( getenv( "HOME" ) );
         else if( argc == 2 ){
-            if( chdir( argv[1] ) )
+            if( chdir(argv[1])!=0 )
                 printf( "No directory\n" );
-        }
+				}
         else
             printf( "USAGE: cd [dir]\n" );
+		return true;
 }
 
 bool cmd_exit( int argc, char* argv[] ){
-        return false;
+       return false; 
 }
 
 bool cmd_help( int argc, char* argv[] ){ // 명령어 출력
@@ -59,6 +61,7 @@ bool cmd_help( int argc, char* argv[] ){ // 명령어 출력
                 if( argc == 1 || strcmp( builtin_cmds[i].name, argv[1] ) == 0 )
                         printf( "%-10s: %s\n", builtin_cmds[i].name, builtin_cmds[i].desc );
         }
+		return true;
 }
 
 bool cmd_myfinger( int argc, char* argv[] ) {
@@ -67,10 +70,29 @@ bool cmd_myfinger( int argc, char* argv[] ) {
 	struct tm *tminfo;
 	char buf[100];
 	int n;
-	char *optarg;
-	int optind;
-		
-	if(argc==1) {
+	int flag_a=0, flag_b=0, flag_c=0;
+	
+	if(argc==2) {
+		n=*(argv[1]+1);
+		switch(n) {
+			case 'a':
+				flag_a=1;	
+				break;
+			case 'b':
+				flag_b=1;	
+				break;
+			case 'c':
+				flag_c=1;	
+				break;
+			default :
+				printf( "USAGE: myfinger [option]\n" );
+				printf( "Available option : a,b,c\n");
+				printf( "USAGE SAMPLE : myfinger -a\n");
+				break;
+		}
+	}
+
+	if(argc==1 || flag_a) {
 	printf("%-10s %-12s %-11s %-11s %-18s\n","Login", "Name", "TTY", "When", "Where"); 
 
 	while((utx=getutxent()) != NULL ) {
@@ -83,31 +105,65 @@ bool cmd_myfinger( int argc, char* argv[] ) {
 
 		printf("%-10s %-12s %-10s %-12s %-18s\n",utx->ut_user, pw->pw_comment, utx->ut_line, buf, utx->ut_host);
 		}
+	endutxent();
 	}
-	if((n=getopt(argc, argv, "ab")) != -1) {
-		switch(n) {
-			case 'a':
-				printf("Option : a\n");
-				break;
-			case 'b':
-				printf("Option : b\n");
-				break;
-			default :
-				printf("Nothing : no\n");
-				break;
+	return true;
+}
+
+bool cmd_mkdir( int argc, char* argv[] ) {
+	if(argc!=2) {
+		printf( "USAGE: mkdir [dir]\n" );
+	}	
+	char wd[BUFSIZ];
+
+	getcwd(wd,BUFSIZ);
+	chdir(wd);
+
+	mkdir(argv[1],0755);
+
+	return true;
+}
+
+bool cmd_rmdir( int argc, char* argv[] ) {
+	char wd[BUFSIZ];
+
+	getcwd(wd,BUFSIZ);
+	chdir(wd);
+
+	if(rmdir(argv[1])==-1) {
+		printf("%s not exist\n");
+	}
+	return true;
+}
+
+bool cmd_ls( int argc, char* argv[] ) {
+	DIR *dir;
+	struct dirent *dent;
+		
+	if((dir=opendir("./"))==NULL) {
+		perror("opendir");
+		exit(1);
+	}
+
+	while(dent=readdir(dir)) {
+		if(dent->d_name[0]=='.') continue;
+		else {
+			if(dent->d_ino !=0) printf("%s ", dent->d_name);
 		}
 	}
+	printf("\n");
+	closedir(dir);
 }
 
 int tokenize( char* buf, char* delims, char* tokens[], int maxTokens ){
-        int token_count = 0;
-        char* token;
-        token = strtok( buf, delims );
-        while( token != NULL && token_count < maxTokens ){
-                tokens[token_count] = token;
-                token_count++;
-                token = strtok( NULL, delims );
-        }
+		int token_count = 0;
+		char* token;	
+		token = strtok( buf, delims );	
+		while( token != NULL && token_count < maxTokens ){
+				tokens[token_count]=token;
+				token_count++;
+				token=strtok(NULL, delims);
+		}
         tokens[token_count] = NULL;
         return token_count;
 }
@@ -117,49 +173,35 @@ bool run( char* line ){
         char* tokens[128];
         int token_count;
         int i;
-        bool back;
-        for(i=0;i<strlen(line);i++){ //background 실행은 wait하지 않는다.
+
+        for(i=0;i<strlen(line);i++){ 
                 if(line[i] == '&'){
-                        back=true;
                         line[i]='\0';
                         break;
                 }
         }
+		
         token_count = tokenize( line, delims, tokens, sizeof( tokens ) / sizeof( char* ) );
-        if( token_count == 0 )
+		if( token_count == 0 )
                 return true;
         for( i = 0; i < sizeof( builtin_cmds ) / sizeof( struct COMMAND ); i++ ){
                 if( strcmp( builtin_cmds[i].name, tokens[0] ) == 0 )
                         return builtin_cmds[i].func( token_count, tokens );
         }
-        child = fork();
-        if( child == 0 ){
-                execvp( tokens[0], tokens );
-                printf( "No such file\n" );
-                _exit( 0 );
-        }
-        else if( child < 0 )
-        {
-            printf( "Failed to fork()!" );
-            _exit( 0 );
-        }
-        else if(back == false){
-            waitpid(child, &status, WUNTRACED );
-        }
-        return true;
 }
 
 int main(){
         char line[1024];
 		char wd[BUFSIZ];
 
-		getcwd(wd,BUFSIZ);
         while( 1 )
         {
+			getcwd(wd,BUFSIZ);
             printf( "[%s] $ ", wd);
-            fgets( line, sizeof( line ) - 1, stdin );
+			fgets(line, sizeof(line), stdin); 
             if( run( line ) == false )
 				break;
+			memset(wd,NULL,BUFSIZ);	
 		}
 		return 0;
 }
