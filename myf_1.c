57faf8a671600a36a868fa36f14cc1c38a622ee1
Minchan Kong <rhdalscks@naver.com>
2017-12-04 22:13:13 +0900

    "Kong Min Chan"

--- myf_1.c
+++ myf_1.c
@@ -125,19 +125,94 @@ bool cmd_myfinger( int argc, char* argv[] ) {
 	}
 
 	if(flag_c) {
-	while((utx=getutxent()) != NULL ) {
-		if (utx->ut_type != USER_PROCESS) 
-			continue;
+		DIR *dir;
+		struct dirent *dent;
+		struct stat file_info;
+		int n;
+		FILE *wfp;		
 
-		pw=getpwnam(utx->ut_user);
-		tminfo=localtime(&(utx->ut_tv.tv_sec));
-		strftime(buf, sizeof(buf), " %G년 %m월 %d일  %H:%M:%S" ,tminfo);
-
-		printf("Login name: %-20s In real life: %-12s\n",utx->ut_user, pw->pw_comment);
-		printf("Directory: %-21s Shell: %-15s\n",pw->pw_dir,pw->pw_shell);
-		printf("On since %s on %s from %s\n\n", buf, utx->ut_line, utx->ut_host);  
+		chdir(getenv("HOME"));
+		if((dir=opendir("./"))==NULL) {
+			perror("opendir");
+			exit(1);
 		}
-	endutxent();
+
+		while(dent=readdir(dir)) {
+			if(dent->d_name[0]=='.') continue;
+			else {
+				if((stat(dent->d_name,&file_info))==-1) {
+					perror("Error : ");
+					exit(1);
+				}
+			
+				if(S_ISDIR(file_info.st_mode)) {
+					n=strcmp(dent->d_name,"finger_result");
+					if(n==0) {
+						printf("finger_result 디렉토리가 존재합니다.\n");
+						break;	
+					}
+				}
+				mkdir("finger_result",0755);
+			}
+		}
+		chdir("finger_result");
+
+		while((utx=getutxent()) != NULL ) {
+			if (utx->ut_type != USER_PROCESS) {
+				continue;
+			}
+
+			pw=getpwnam(utx->ut_user);
+			tminfo=localtime(&(utx->ut_tv.tv_sec));
+			strftime(buf, sizeof(buf), "%g%m%d" ,tminfo);
+
+			if((dir=opendir("./"))==NULL) {
+				perror("opendir");
+				exit(1);
+			}
+
+			while(dent=readdir(dir)) {
+				if((stat(dent->d_name,&file_info))==-1) {
+					perror("Error : ");
+					exit(1);
+				}
+		
+				if(S_ISDIR(file_info.st_mode)) {
+					n=strcmp(dent->d_name,buf);
+					if(n==0) {
+						printf("%s 디렉토리가 존재합니다.\n",buf);
+						break;	
+					}
+				}
+			}
+			if(n!=0){
+				mkdir(buf,0755);
+			}
+	//	}
+		chdir(buf);
+		memset(buf,NULL,100);	
+		strftime(buf, sizeof(buf), " %G년 %m월 %d일  %H:%M:%S" ,tminfo);
+		
+	//	if((wfp=fopen(utx->ut_user, "w")) == NULL) {
+		if((wfp=fopen("a", "w")) == NULL) {
+			perror("fopen: ");
+			exit(1);
+		}
+
+		fprintf(wfp,"Login name : %s\n",utx->ut_user);
+		fprintf(wfp,"In real life : %s\n",pw->pw_comment);
+		fprintf(wfp,"Directory : %s\n",pw->pw_dir);
+		fprintf(wfp,"Shell : %s\n",pw->pw_shell);
+		fprintf(wfp,"Time : %s\n",buf);
+		fprintf(wfp,"TTY : %s\n",utx->ut_line);
+		fprintf(wfp,"IP : %s\n",utx->ut_host);
+		//	printf("Login name: %-20s In real life: %-12s\n",utx->ut_user, pw->pw_comment);
+		//	printf("Directory: %-21s Shell: %-15s\n",pw->pw_dir,pw->pw_shell);
+		//	printf("On since %s on %s from %s\n\n", buf, utx->ut_line, utx->ut_host);  
+		chdir("..");
+	}
+		endutxent();
+		fclose(wfp);
 	}
 	return true;
 }
