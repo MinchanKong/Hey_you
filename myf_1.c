23c64655177eef45a8b6573c733a56694547e81b
MinchanKong <33535104+MinchanKong@users.noreply.github.com>
2017-12-05 10:31:30 +0900

    Update myf_1.c
    "Completed"

--- myf_1.c
+++ myf_1.c
@@ -14,7 +14,7 @@
 #include <sys/wait.h>
 #include <siganl.h>
 
-struct COMMAND{ // 커맨드 구조체
+struct COMMAND { // 커맨드 구조체
         char* name;
         char* desc;
         bool ( *func )( int argc, char* argv[] ); // 함수포인터. 사용할 함수들의 매개변수를 맞춰줌
@@ -77,11 +77,10 @@ void handler(int sig)
 
 bool cmd_pwd (int argc, char *argv[])
 {
-	char buf[BUFSIZ];
-	int bufdize;
+	char wd[BUFSIZ];
 	
-	getcwd(buf,bufdize);
-	printf("%s\n",buf);
+	getcwd(wd,BUFSIZ);
+	printf("%s\n",wd);
 	
 	return true;
 }
@@ -357,23 +356,145 @@ bool cmd_rmdir( int argc, char* argv[] ) {
 	return true;
 }
 
-bool cmd_ls( int argc, char* argv[] ) {
-	DIR *dir;
-	struct dirent *dent;
-		
-	if((dir=opendir("./"))==NULL) {
-		perror("opendir");
-		exit(1);
+void rwx(file_mode) {
+	if(S_ISDIR(file_mode)) {
+		printf("d");
 	}
-
-	while(dent=readdir(dir)) {
-		if(dent->d_name[0]=='.') continue;
-		else {
-			if(dent->d_ino !=0) printf("%s ", dent->d_name);
+	
+	else printf("-");
+	
+	if(file_mode&S_IRUSR) {
+		printf("r");
+	}
+	else printf("-");
+	
+	if(file_mode&S_IWUSR) {
+		printf("w");
+	}
+	else printf("-");
+	
+	if(file_mode&S_IXUSR) {
+		printf("x");
+	}
+	else printf("-");
+	
+	if(file_mode&S_IRGRP) {
+		printf("r");
+	}
+	else printf("-");
+	
+	if(file_mode&S_IWGRP) {
+		printf("w");
+	}
+	else printf("-");
+	
+	if(file_mode&S_IXGRP) {
+		printf("x");
+	}
+	else printf("-");
+	
+	if(file_mode&S_IROTH) {
+		printf("r");
+	}
+	else printf("-");
+	
+	if(file_mode&S_IWOTH) {
+		printf("w");
+	}
+	else printf("-");
+	
+	if(file_mode&S_IXOTH) {
+		printf("x");
+	}
+	else printf("-");
+	
+	printf(" ");
+}
+		
+bool cmd_ls( int argc, char* argv[] ) {
+	int flag_l=0;
+	
+	if(argc==2) {
+		n=*(argv[1]+1);
+		switch(n) {
+			case 'l':
+				flag_l=1;	
+				break;
+			default :
+				printf( "USAGE: myfinger [option]\n" );
+				printf( "Available option : l\n");
+				printf( "USAGE SAMPLE : ls -l\n");
+				break;
 		}
 	}
-	printf("\n");
-	closedir(dir);
+
+	if(argc==1) {
+		DIR *dir;
+		struct dirent *dent;
+
+		if((dir=opendir("./"))==NULL) {
+			perror("opendir");
+			exit(1);
+		}
+
+		while(dent=readdir(dir)) {
+			if(dent->d_name[0]=='.') continue;
+			else {
+				if(dent->d_ino !=0) printf("%s ", dent->d_name);
+			}
+		}
+		printf("\n");
+		closedir(dir);
+	}
+	
+	if(flag_l) {
+		struct passwd *pw;
+		DIR *dirp;
+		struct dirent *dentry;
+		struct group *group;
+		struct stat file_info;
+		mode_t file_mode;
+		struct tm *tminfo;
+		int return_stat;
+		char buf[100];
+		
+		if((dirp=opendir("./"))==NULL) {
+			exit(1);
+		}
+		
+		while(dentry=readdir(dirp)) {
+			if((return_stat=stat(dentry->d_name, &file_info))==-1) {
+				perror("Error : ");
+				exit(0);
+			}
+			
+			if(dentry->d_name[0]=='.') {
+				continue;
+			}
+			
+			else {
+				file_mode=file.info.st_mode;
+				rwx(file_mode);
+				printf(" %d ", file_info.st_nlink);
+				
+				pw=getpwuid(file_info.st_uid);
+				group=getgrgid(file_info.st_gid);
+				printf("%s", pw->pw_name);
+				printf(" %s ", group->gr_name);
+				
+				printf(" %d ",file_info.st_size);
+				
+				tminfo=localtime(&(file_info.st_mtime));
+				strftime(buf,sizeof(buf)," %m월 %d일 %H:%M ",tminfo);
+				printf(" %s ",buf);
+				
+				if(dentry->d_ino !=0) {
+					printf(" %s\n",dentry->d_name);
+				}
+			}
+			closedir(dirp);
+		}
+	} 
 }
 
 int tokenize( char* buf, char* delims, char* tokens[], int maxTokens ){
